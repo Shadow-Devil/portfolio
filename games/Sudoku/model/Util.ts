589d9a04c8311cd1d93bcd40577b5cec5c955bee
fschn <fschnabel007@gmail.com>
2021-03-03 10:52:23 +0100

    performance improve of initialization of the Sudoku

--- games/Sudoku/model/Util.ts
+++ games/Sudoku/model/Util.ts
@@ -8,18 +8,20 @@ const EMPTY  = 0;
  */
 const randomInt = (max: number): number => Math.floor(Math.random() * max);
 
-export const emptySudoku = (): Sudoku => {
-    return Array.from(new Array(9), () => new Array(9));
-}
+export const emptySudoku = (): Field[][] => Array.from(new Array(9), () => new Array<Field>(9));
+
+
+const emptySudokuIntern = (): number[][] =>  Array.from(new Array(9), () => new Array<number>(9));
+
 
 const isInDiff = (diff: Difficulty, x: number) => x >= diff.min && x <= diff.max;
 
-export function produceGameBoard(diff: Difficulty) {
+export function produceGameBoard(diff: Difficulty): Sudoku {
     const fullBoard = produceFullBoard();
-    let deleted;
-    let board;
+    let deleted: number;
+    let board: number[][];
     do {
-        board = copy(fullBoard);
+        board = fullBoard.slice();
         deleted = 0;
         let x   = 0;
         let y   = 0;
@@ -27,58 +29,53 @@ export function produceGameBoard(diff: Difficulty) {
         while (uniqueSolution(board) === 1) {
             x = randomInt(9);
             y = randomInt(9);
-            tmp = board[x][y].getContent();
+            tmp = board[x][y];
             if (tmp !== EMPTY) {
-                board[x][y].setInhalt(EMPTY);
+                board[x][y] = EMPTY;
                 deleted++;
             }
         }
-        board[x][y].setInhalt(tmp);
+        board[x][y] = tmp;
     } while (!isInDiff(diff, deleted));
 
-    for (let x = 0; x < board.length; x++) {
-        for (let y = 0; y < board[0].length; y++) {
-            if (board[x][y].getContent() !== 0)
-                board[x][y] = new FinalField(board[x][y].getContent());
-        }
-    }
-
-    return board;
+    return internToObject(board);
 }
 
 /**
  * Zufälliges Füllen mit Abfrage nach Regelverstoß
  */
 function produceFullBoard() {
-    let result = setEmpty(emptySudoku());
-    while (!isFull(result)) {
-        result = ((input: Sudoku) => {
-            for (let x = 0; x < 9; x++) {
-                for (let y = 0; y < 9; y++) {
-                    const r = 1 + randomInt(9);
-                    let runner = r;
-                    input[x][y].setInhalt(r);
-                    while (!pruefeSpielzahl(input, x, y)) {
-                        runner = (runner % 9) + 1;
-                        input[x][y].setInhalt(runner);
+    let result = emptySudokuIntern();
+    while (!isFullIntern(result)) {
+        result = ((input: number[][]) => {
+                    for (let x = 0; x < 9; x++) {
+                        for (let y = 0; y < 9; y++) {
+                            const r = 1 + randomInt(9);
+                            let runner = r;
+                            input[x][y] = r;
+                            while (!canPlaceIntern(input, x, y)) {
+                                runner = (runner % 9) + 1;
+                                input[x][y] = runner;
 
-                        // Spielfeld kann nicht gefüllt werden
-                        if (r === runner) {
-                            return setEmpty(input);
+                                // Spielfeld kann nicht gefüllt werden
+                                if (r === runner) {
+                                    return emptySudokuIntern();
+                                }
+                            }
                         }
                     }
-                }
-            }
-            return input;
-        })(result);
+                    return input;
+                })(result);
     }
     return result;
 }
 
+
+
 /**
  * Prüft eine Zahl nach Regelverstoß
  */
-export function pruefeSpielzahl(board: Sudoku, x: number, y: number) {
+export function pruefeSpielzahl(board: Field[][], x: number, y: number) {
     const current = board[x][y].getContent();
 
     // testen, ob Feld beschrieben
@@ -114,30 +111,66 @@ export function pruefeSpielzahl(board: Sudoku, x: number, y: number) {
     return true;
 }
 
-export const uniqueSolution = (board: Sudoku): number => solve(0, 0, board);
+function canPlaceIntern(board: number[][], x: number, y: number): boolean {
+    const current = board[x][y];
 
-function solve(x: number, y: number, tmpfeld: Sudoku): number {
+    // testen, ob Feld beschrieben
+    if (current === EMPTY)
+        return false;
+
+    // Zeilen und Spalten prüfen
+    for (let i = 0; i < 9; i++) {
+
+        // Zeilenabfrage
+        if (current === board[i][y] && x !== i)
+            return false;
+
+        // Spaltenabfrage
+        if (current === board[x][i] && y !== i)
+            return false;
+
+    }
+    let xtmp = Math.floor(x / 3) * 3; // Welches 3x3
+    let ytmp = Math.floor(y / 3) * 3;
+    const xmax = xtmp + 3;
+    const ymax = ytmp + 3;
+    while (ytmp < ymax)// im 3x3 Quadrat Regelverstoß
+    {
+        while (xtmp < xmax) {
+            if (current === board[xtmp][ytmp] && x !== xtmp && y !== ytmp)
+                return false;
+            xtmp++;
+        }
+        xtmp = x / 3 * 3;
+        ytmp++;
+    }
+    return true;
+}
+
+export const uniqueSolution = (board: number[][]): number => solve(0, 0, board);
+
+function solve(x: number, y: number, board: number[][]): number {
     let counter = 0;
 
-    if (tmpfeld[x][y].getContent() === EMPTY) {
+    if (board[x][y] === EMPTY) {
         for (let i = 1; i <= 9; i++) {
-            tmpfeld[x][y].setInhalt(i);
-            if (pruefeSpielzahl(tmpfeld, x, y)) {
+            board[x][y] = i;
+            if (canPlaceIntern(board, x, y)) {
                 if (x < 8) {
-                    counter = counter + solve(x + 1, y, tmpfeld);
+                    counter += solve(x + 1, y, board);
                 } else if (y < 8) {
-                    counter = counter + solve(0, y + 1, tmpfeld);
+                    counter += solve(0, y + 1, board);
                 } else {
                     return 1;
                 }
             }
         }
-        tmpfeld[x][y].setInhalt(EMPTY);
+        board[x][y] = EMPTY;
     } else {
         if (x < 8) {
-            counter = counter + solve(x + 1, y, tmpfeld);
+            counter += solve(x + 1, y, board);
         } else if (y < 8) {
-            counter = counter + solve(0, y + 1, tmpfeld);
+            counter += solve(0, y + 1, board);
         } else {
             return 1;
         }
@@ -161,7 +194,7 @@ export function isFinished(board: Sudoku): boolean {
 
 
 export const isFull = (board: Sudoku): boolean => andBoard(board, field => field.getContent() !== EMPTY);
-
+const isFullIntern = (board: number[][]): boolean => andBoardIntern(board, field => field !== EMPTY);
 
 export function setEmpty(board: Sudoku): Sudoku {
     for (const row of board)
@@ -175,33 +208,12 @@ export function setEmpty(board: Sudoku): Sudoku {
 /**
  * Creates A new Sudoku with every Field copied.
  */
-function copy(from: Field[][]): Field[][] {
+function internToObject(from: number[][]): Field[][] {
     const to = emptySudoku();
     for (let x = 0; x < from.length; x++)
         for (let y = 0; y < from[0].length; y++)
-            to[x][y] = new Field(from[x][y].getContent());
+            to[x][y] = from[x][y] === EMPTY ? new Field(from[x][y]) : new FinalField(from[x][y]);
     return to;
-}
-
-function ausgeben(f: Sudoku) // Ausgeben des Gesamten Feldes
-{
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            console.log(f[j][i].getContent() + " ");
-
-        }
-        console.log("\n");
-    }
-}
-
-function ausgebenkonstant(f: Sudoku) // Ausgeben des Gesamten Feldes
-{
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            console.log(f[j][i].isConstant() + "\t");
-        }
-        console.log();
-    }
 }
 
 /**
@@ -212,6 +224,14 @@ function ausgebenkonstant(f: Sudoku) // Ausgeben des Gesamten Feldes
  * @return True if all Fields return true on the Pradicate else false.
  */
 export function andBoard(board: Sudoku, p: (field: Field) => boolean) {
+    for (const column of board)
+        for (const field of column)
+            if (!p(field))
+                return false;
+    return true;
+}
+
+function andBoardIntern(board: number[][], p: (field: number) => boolean) {
     for (const column of board)
         for (const field of column)
             if (!p(field))
