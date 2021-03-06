bd1b72be4f7d8339db6e370eae9a1890b2030b66
fschn <fschnabel007@gmail.com>
2021-03-06 15:59:37 +0100

    Changed HARD min and max

--- games/Sudoku/model/Types.ts
+++ games/Sudoku/model/Types.ts
@@ -15,5 +15,5 @@ class Dif {
 
 export const EASY = new Dif(45, 51);
 export const MEDIUM = new Dif(50, 56);
-export const HARD = new Dif(55, 57);
+export const HARD = new Dif(57, 70);
 export const EXTREME = new Dif(57, 68);
